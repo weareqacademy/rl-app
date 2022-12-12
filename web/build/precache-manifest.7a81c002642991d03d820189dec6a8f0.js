b111775b7add81ed114ca91f25a95f6015fb7e1b
Fernando Papito <fernando.papini@pagueveloz.dev>
2022-12-12 18:17:04 -0300

    go fly

---
+++ web/build/precache-manifest.7a81c002642991d03d820189dec6a8f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d215ba65804cf09e952a1f3657e9964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb5e8731123f83ac53b",
+    "url": "/static/css/2.41ff332e.chunk.css"
+  },
+  {
+    "revision": "550dd8639b6c5403812f",
+    "url": "/static/css/main.40ee7a32.chunk.css"
+  },
+  {
+    "revision": "aeb5e8731123f83ac53b",
+    "url": "/static/js/2.8843da06.chunk.js"
+  },
+  {
+    "revision": "550dd8639b6c5403812f",
+    "url": "/static/js/main.2115bb9c.chunk.js"
+  },
+  {
+    "revision": "a2806f7ab0fa17a92097",
+    "url": "/static/js/runtime~main.dc128e9d.js"
+  },
+  {
+    "revision": "89a82e50a0be9a7da9b6b0f242a466af",
+    "url": "/static/media/background.89a82e50.jpg"
+  },
+  {
+    "revision": "234a4ab9e44a4f0dc0b8ada21c697b02",
+    "url": "/static/media/camera.234a4ab9.svg"
+  },
+  {
+    "revision": "63e509651d07f5173524a784ce7faa02",
+    "url": "/static/media/default.63e50965.svg"
+  },
+  {
+    "revision": "f6ac8fdfb3e7e49d006ca40260d271ff",
+    "url": "/static/media/ico-master.f6ac8fdf.webp"
+  },
+  {
+    "revision": "5dcbb2831a923a074f71919731cc3d79",
+    "url": "/static/media/logo.5dcbb283.svg"
+  },
+  {
+    "revision": "84384ca33e426883316841eb48c40016",
+    "url": "/static/media/oops.84384ca3.svg"
+  }
+]);
